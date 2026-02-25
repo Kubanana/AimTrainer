c3ff8c421f3a4a201e30ab8b1c67def8d5e6d2df
unknown <elcia116ee@gmail.com>
2026-02-25 17:43:01 +0000

    Basic window setup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,25 @@
-﻿public class Program
+﻿using Silk.NET.Windowing;
+
+public class Program
 {
-    
+    static void Main()
+    {
+        var options = WindowOptions.DefaultVulkan;
+        options.Size = new Silk.NET.Maths.Vector2D<int>(1280, 720);
+        options.Title = "AimTrainer";
+
+        var window = Window.Create(options);
+
+        window.Load += () =>
+        {
+            Console.WriteLine("Window Created");  
+        };
+
+        window.Render += delta =>
+        {
+            
+        };
+
+        window.Run();
+    }
 }
